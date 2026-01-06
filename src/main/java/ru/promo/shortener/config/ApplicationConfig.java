cc55767612d6acea0df5a998e593005f1c0eeb1a
Kseniia Simak <pakhitovak@yandex.ru>
2026-01-06 16:32:28 +0300

    config

--- src/main/java/ru/promo/shortener/config/ApplicationConfig.java
+++ src/main/java/ru/promo/shortener/config/ApplicationConfig.java
@@ -1,27 +1,26 @@
 package ru.promo.shortener.config;
 
 public class ApplicationConfig {
-
-    public final long ttlSeconds;
-    public final int defaultMaxClicks;
-
     public final int initialKeyLength;
     public final int maxKeyLength;
     public final int attemptsPerLength;
 
+    public final long ttlSeconds;
+    public final int defaultMaxClicks;
+
     public final int cleanupIntervalSeconds;
 
-    public ApplicationConfig(long ttlSeconds,
-                             int defaultMaxClicks,
-                             int initialKeyLength,
+    public ApplicationConfig(int initialKeyLength,
                              int maxKeyLength,
                              int attemptsPerLength,
+                             long ttlSeconds,
+                             int defaultMaxClicks,
                              int cleanupIntervalSeconds) {
-        this.ttlSeconds = ttlSeconds;
-        this.defaultMaxClicks = defaultMaxClicks;
         this.initialKeyLength = initialKeyLength;
         this.maxKeyLength = maxKeyLength;
         this.attemptsPerLength = attemptsPerLength;
+        this.ttlSeconds = ttlSeconds;
+        this.defaultMaxClicks = defaultMaxClicks;
         this.cleanupIntervalSeconds = cleanupIntervalSeconds;
     }
 }
--- src/main/java/ru/promo/shortener/config/ApplicationConfigLoader.java
+++ src/main/java/ru/promo/shortener/config/ApplicationConfigLoader.java
@@ -8,26 +8,32 @@ public class ApplicationConfigLoader {
     public static ApplicationConfig load() {
         Properties props = new Properties();
 
-        try (InputStream is = ApplicationConfigLoader.class
-                .getClassLoader()
+        try (InputStream is = ApplicationConfigLoader.class.getClassLoader()
                 .getResourceAsStream("application.properties")) {
-
             if (is == null) {
-                throw new IllegalStateException("application.properties not found");
+                throw new IllegalStateException("application.properties not found in resources");
             }
-
             props.load(is);
         } catch (Exception e) {
             throw new IllegalStateException("Failed to load application.properties", e);
         }
 
+        int initialKeyLength = Integer.parseInt(props.getProperty("shortkey.length.initial"));
+        int maxKeyLength = Integer.parseInt(props.getProperty("shortkey.length.max"));
+        int attemptsPerLength = Integer.parseInt(props.getProperty("shortkey.attempts.per.length"));
+
+        long ttlSeconds = Long.parseLong(props.getProperty("link.ttl.seconds"));
+        int defaultMaxClicks = Integer.parseInt(props.getProperty("link.default.max-clicks"));
+
+        int cleanupIntervalSeconds = Integer.parseInt(props.getProperty("cleanup.interval.seconds"));
+
         return new ApplicationConfig(
-                Long.parseLong(props.getProperty("link.ttl.seconds")),
-                Integer.parseInt(props.getProperty("link.default.max-clicks")),
-                Integer.parseInt(props.getProperty("shortkey.length.initial")),
-                Integer.parseInt(props.getProperty("shortkey.length.max")),
-                Integer.parseInt(props.getProperty("shortkey.attempts.per.length")),
-                Integer.parseInt(props.getProperty("cleanup.interval.seconds"))
+                initialKeyLength,
+                maxKeyLength,
+                attemptsPerLength,
+                ttlSeconds,
+                defaultMaxClicks,
+                cleanupIntervalSeconds
         );
     }
 }
